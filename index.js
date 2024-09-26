d8da2a6877b7e7d22bd6a96efdece7f2b08ec6b9
luqman-bashir <uthamin98@gmail.com>
2024-09-26 16:01:25 +0300

    arithmetic-lab

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
+let num1 = 62;  
+let num2 = 1;  
 
-// Write your code here
+let multiply = num1 * num2; 
+var random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1; 
+
+
+let num3 = 0; 
+let num4 = 0;
+
+
+num3 = num4 * someInteger + 4;  
+
+num4 = 10;   
+let someInteger = Math.floor((Math.random() * 10));   
+num3 = num4 * someInteger + 4;  
+
+let mod = num3 % num4; 
+console.log(mod); 
+
+  
+const numbers = [10, 15, 20, 5, 7];  
+const max = Math.max(...numbers);   
+console.log(max); 
